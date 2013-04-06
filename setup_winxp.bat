5fdf5b8f0aff1ed8e02029034e7eb903fcad881f
ayechan <ayechan0123@gmail.com>
2013-04-06 18:11:43 +0900

    add more support for xp

--- setup_winxp.bat
+++ setup_winxp.bat
@@ -4,3 +4,7 @@ fsutil hardlink create %HOME%\.vimrc %HOME%\dotfiles\vim\vimrc.vim
 fsutil hardlink create %HOME%\.gvimrc %HOME%\dotfiles\vim\gvimrc.vim
 fsutil hardlink create %HOME%\.zshrc %HOME%\dotfiles\sh\zshrc.zsh
 fsutil hardlink create %HOME%\.zshenv %HOME%\dotfiles\sh\zshenv.zsh
+fsutil hardlink create %HOME%\.inputrc %HOME%\dotfiles\rcfiles\.inputrc
+fsutil hardlink create %HOME%\.bash_profile %HOME%\dotfiles\sh\bash_profile.bash
+fsutil hardlink create %HOME%\.bashrc %HOME%\dotfiles\sh\bashrc.bash
+
