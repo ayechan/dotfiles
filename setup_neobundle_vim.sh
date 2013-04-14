76f9b166c45758c6f85a074840b3fb6e86d10519
ayechan <ayechan0123@gmail.com>
2013-04-14 20:40:46 +0900

    automate install/update bundles

--- setup_neobundle_vim.sh
+++ setup_neobundle_vim.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
-# This script initializes neobundle.vom
+# This script initializes neobundle.vim
 
-if [ -d $HOME/.vim/bundle/neobundle.vim ]; then
-  echo "NeoBundle.vim exists already."
-elif type "git" >/dev/null 2>&1 ; then
+if ! type "git" >/dev/null 2>&1 ; then
+  echo "This script needs git."
+  exit 1;
+fi
+
+if [ ! -d $HOME/.vim/bundle/neobundle.vim ]; then
+  echo "cloning NeoBundle.vim"
   mkdir -vp $HOME/.vim/bundle
   pushd $HOME/.vim/bundle
   git clone https://github.com/Shougo/neobundle.vim
   popd
-else
-  echo "This script needs git."
-  exit 1;
 fi
+
+if [ -d $HOME/.vim/bundle/neobundle.vim ]; then
+  vim -c "NeoBundleInstall | quit"
+  if [ -d $HOME/.vim/bundle/vimproc ]; then
+    pushd $HOME/.vim/bundle/vimproc
+    VIMPROC_MAK=""
+    case "${OSTYPE}" in
+      linux*) VIMPROC_MAK='make_unix.mak' ;;
+      cygwin*) VIMPROC_MAK='make_cygwin.mak' ;;
+      darwin*) VIMPROC_MAK='make_mac.mak' ;;
+      *) VIMPROC_MAK='' ;;
+    esac
+    if [ ! -z $VIMPROC_MAK ]; then
+      make -f $VIMPROC_MAK
+    fi
+    popd
+  fi
+  vim -c "NeoBundleUpdate | quit"
+fi
+
+
